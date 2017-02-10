9440f5f65719fdcf709d2441ef5e854f9a28658a
Nenba Jonathan <jonathannenba@gmail.com>
2017-02-10 03:20:50 +0100

    update index.php to add the author link

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         
         <script src="function.js" type="text/javascript"></script>        
         
-		<title>Saange - Tirage au sort FSDeanCup</title>
+		<title>Saange - Tirage au sort des equipe aleatoirement</title>
 	</head>
 	<body>
         <nav class="navbar navbar-default" role="navigation">
@@ -36,6 +36,31 @@
                 include_once('programme.php')
             ?>
 			<section class="container">
+		
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  				About @uthor !
+			</button>
+
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+			      </div>
+			      <div class="modal-body">
+				...
+			      </div>
+			      <div class="modal-footer">
+				<button type="button" class= "btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+	
                 <h6>Appliction de tirage d'equipe aleatoire.</h6>
                 <?php if(isset($flash)){ echo '<div class="alert alert-danger">'.$flash.'</div>' ; } ?>
                 <form action="" method="POST">
@@ -182,7 +207,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <ul>
-                        <li><a href="">Aller à la FSDeanCup</a></li>
+                        <li><a href="">A propos de l'Auteur</a></li>
                     </ul>
                 </div>
             </div>
